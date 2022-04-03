684c67c68755d3121c5735b17e49050ff5404b34
amalia <amalia.reodica@gmail.com>
2022-04-03 19:07:25 -0400

    add name validation to welcome page

--- src/components/ghost/Welcome.js
+++ src/components/ghost/Welcome.js
@@ -6,15 +6,15 @@ import {Context} from '../../Context'
 export default function Welcome() {    
     const {input} = React.useContext(Context) 
     
-    const name = input.firstName[0].toUpperCase() + input.firstName.substring(1).toLowerCase();
+    const name = input.firstName ? input.firstName[0].toUpperCase() + input.firstName.substring(1).toLowerCase() + ' ' : '';
 
 
     return (
         <section className="welcome wrapper">
             <div class="container">
                 <div class="glitch" data-text={`Welcome 
-                ${name + ' '}to Hotel Del Luna!`}>Welcome {name + ' '}to Hotel Del Luna!</div>
-                <div class="glow">Welcome {name + ' '}to Hotel Del Luna!</div>
+                ${name}to Hotel Del Luna!`}>Welcome {name}to Hotel Del Luna!</div>
+                <div class="glow">Welcome {name}to Hotel Del Luna!</div>
 
             </div>
             <div class="scanlines"></div>
